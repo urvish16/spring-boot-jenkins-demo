3cc2e6777c61abb8215640333c50b16f1a4c1c52
urvish16 <urvishsoni96@gmail.com>
2025-08-23 10:59:53 +0530

    updated Hello message

--- src/main/java/com/spring_boot_jenkins_demo/controller/HelloController.java
+++ src/main/java/com/spring_boot_jenkins_demo/controller/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/hello")
     public String sayHello() {
-        return "Hello from Spring Boot CI/CD Demo!";
+        return "Updated: Hello from Spring Boot CI/CD Demo!";
     }
 }
